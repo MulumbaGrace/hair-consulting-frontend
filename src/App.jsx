7ce162ab7453963cb686e7d935896a9226fbe771
MulumbaGrace <gracemulumba@hotmail.com>
2024-06-05 00:59:46 -0400

    add consultation page, background picture

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
+import Consultation from "./pages/Consultation/Consultation";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/consultation" element={<Consultation />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import './Header.scss';
 import logo from '../../assets/logo/logo2.png'; 
 
+
 const Header = () => {
   return (
     <header className="header">
       <div className="header__logo">
-        <img src={logo} alt="Hair Consulting Logo" />
+        <Link to="/"><img src={logo} alt="Hair Consulting Logo" /></Link>
       </div>
       <div className="header__link">
         <Link to="/consultation" className="header__consultation-link">
