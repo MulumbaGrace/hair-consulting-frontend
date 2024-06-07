1e8d6dde8bea61c928034693ea13253a6cd0f448
MulumbaGrace <gracemulumba@hotmail.com>
2024-06-07 00:32:43 -0400

    error

--- src/pages/Submission/SubmissionPage.jsx
+++ src/pages/Submission/SubmissionPage.jsx
@@ -3,7 +3,7 @@ import './SubmissionPage.scss';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
-const SubmissionPage = () => {
+const faq = () => {
     const [activeIndex, setActiveIndex] = useState(null);
     const faqData = [
     {
